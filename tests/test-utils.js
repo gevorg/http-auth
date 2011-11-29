a365d7592a2c2c295e5c4e57b049c28214c7cbd0
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2011-11-29 23:39:56 +0400

    Line formating added!

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -3,43 +3,42 @@
  */
 var utils = require('../lib/utils');
 
-
 /**
  * Base64 test with ASCII.
  */
-exports['testBase64ASCII'] = function (test) {
+exports['testBase64ASCII'] = function(test) {
 	// Checking encoded string.
 	test.equal(utils.base64("some text"), "c29tZSB0ZXh0", "ASCII string is not encoded correctly!");
 	// Test is done.
 	test.done();
 };
-
 /**
  * Base64 test with unicode.
  */
-exports['testBase64Unicode'] = function (test) {
+exports['testBase64Unicode'] = function(test) {
 	// Checking encoded string.
-	test.equal(utils.base64("այսպես"), "1aHVtdW91brVpdW9", "Unicode string is not encoded correctly!");
+	test.equal(utils.base64("այսպես"), "1aHVtdW91brVpdW9", 
+		"Unicode string is not encoded correctly!");
 	// Test is done.
 	test.done();
 };
-
 /**
  * MD5 test with ASCII.
  */
-exports['testMD5ASCII'] = function (test) {
+exports['testMD5ASCII'] = function(test) {
 	// Checking generated hash.
-	test.equal(utils.base64("other text"), "b3RoZXIgdGV4dA==", "MD5 hash is not correct for ASCII string!");
+	test.equal(utils.base64("other text"), "b3RoZXIgdGV4dA==", 
+		"MD5 hash is not correct for ASCII string!");
 	// Test is done.
 	test.done();
 };
-
 /**
  * MD5 test with unicode.
  */
-exports['testMD5Unicode'] = function (test) {
+exports['testMD5Unicode'] = function(test) {
 	// Checking generated hash.
-	test.equal(utils.base64("այնպես"), "1aHVtdW21brVpdW9", "MD5 hash is not correct for unicode string!");
+	test.equal(utils.base64("այնպես"), "1aHVtdW21brVpdW9", 
+		"MD5 hash is not correct for unicode string!");
 	// Test is done.
 	test.done();
 };
