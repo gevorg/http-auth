0bc60436818acd650ae681020191e2bda6887176
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2011-12-03 16:13:14 +0400

    Tests added for Basic auth module.

--- tests/auth/test-basic.js
+++ tests/auth/test-basic.js
@@ -26,8 +26,16 @@ exports['setUp'] = function(callback) {
 	source = new Basic("AweSome REALM", ["userhash1", "userhash2"]);
 	// GOD knows why I need to call this.
 	callback();
-}
-
+};
+/**
+ * TearDown.
+ */
+exports['tearDown'] = function(callback) {
+	// Deletes basic instance after each test.
+	delete source;
+	// GOD knows why I need to call this.
+	callback();
+};
 /**
  * Test for ask.
  */
@@ -42,6 +50,77 @@ exports['testAsk'] = function(test) {
 	
 	// Source method call.
 	source.ask(response);
+	
+	// Asserts all mock expectations.
+	response.assert();
+	// Test is done.
+	test.done();
+};
+/**
+ * Test for isAuthenticated, true case.
+ */
+exports['testIsAuthenticatedTrue'] = function(test) {
+	// Initiates input request.
+	var request = {headers : {authorization : "Basic: userhash1"}};
+		
+	// Source method call, that must return true.
+	test.ok(source.isAuthenticated(request), "User must be valid!");
+	
+	// Test is done.
+	test.done();
+};
+/**
+ * Test for isAuthenticated, false case.
+ */
+exports['testIsAuthenticatedFalse'] = function(test) {
+	// Initiates input request.
+	var request = {headers : {authorization : "Basic: userhash4"}};
+		
+	// Source method call, that must return false.
+	test.ok(!source.isAuthenticated(request), "User must not be valid!");
+	
+	// Test is done.
+	test.done();
+};
+/**
+ * Test for apply, pass case.
+ */
+exports['testApplyPass'] = function(test) {
+	// Initiates input request.
+	var request = {headers : {authorization : "Basic: userhash1"}};
+	// Initiates response.
+	var response = {};
+	// Initiates callback.
+	var callbackMock = nodemock.mock("mockMethod").takes("YOU MUST CALL ME!");
+	
+	// Source method call.
+	source.apply(request, response, function() {
+		callbackMock.mockMethod("YOU MUST CALL ME!");
+	});
+	
+	// Asserts all mock expectations.
+	callbackMock.assert();
+	// Test is done.
+	test.done();
+};
+/**
+ * Test for apply, auth case.
+ */
+exports['testApplyAuth'] = function(test) {
+	// Initiates input request.
+	var request = {headers : {authorization : "Basic: userhash4"}};
+	// Response mock(setHeader).
+	var response = nodemock.mock("setHeader").takes("WWW-Authenticate", 
+		"Basic realm=\"AweSome REALM\"");
+	// Response mock(writeHead).
+	response.mock("writeHead").takes(401);
+	// Response mock(end).
+	response.mock("end").takes(defaults.HTML_401);
+	
+	// Source method call.
+	source.apply(request, response, function() {
+		test.ok(false, "Callback should not be called!");
+	});
 	
 	// Asserts all mock expectations.
 	response.assert();
