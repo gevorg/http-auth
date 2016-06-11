d210a9fe1711796ebcc49e5bd8321ae397d8bc4f
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2016-06-11 17:38:28 +0400

    Digest tests.

---
+++ test/digest.js
@@ -0,0 +1,74 @@
+"use strict";
+
+// Expect module.
+import {expect} from 'chai'
+
+// Request module.
+import request from 'request'
+
+// Source.
+import auth from '../gensrc/http-auth'
+
+// HTTP.
+import http from 'http'
+
+// Digest auth.
+describe('digest', function () {
+    let server = undefined;
+
+    before(function() {
+        // Configure authentication.
+        let digest = auth.digest({
+            realm: "Simon Area.",
+            file: __dirname + "/../data/users.htdigest"
+        });
+
+        // Creating new HTTP server.
+        server = http.createServer(digest, function (req, res) {
+            res.end(`Welcome to private area - ${req.user}!`);
+        });
+
+        // Start server.
+        server.listen(1337);
+    });
+
+    after(function() {
+        server.close();
+    });
+
+    it('success', function () {
+        let callback = function(error, response, body) {
+            expect(body).to.equal("Welcome to private area - vivi!");
+        };
+
+        // Test request.
+        request.get('http://127.0.0.1:1337', callback).auth('vivi', 'anna', false);
+    });
+
+    it('special uri', function () {
+        let callback = function(error, response, body) {
+            expect(body).to.equal("Welcome to private area - vivi!");
+        };
+
+        // Test request.
+        request.get('http://127.0.0.1:1337/?coffee=rocks', callback).auth('vivi', 'anna', false);
+    });
+
+    it('wrong password', function () {
+        let callback = function(error, response, body) {
+            expect(body).to.equal("401 Unauthorized");
+        };
+
+        // Test request.
+        request.get('http://127.0.0.1:1337', callback).auth('vivi', 'goose', false);
+    });
+
+    it('wrong user', function () {
+        let callback = function(error, response, body) {
+            expect(body).to.equal("401 Unauthorized");
+        };
+
+        // Test request.
+        request.get('http://127.0.0.1:1337', callback).auth('brad', 'anna', false);
+    });
+});
